89489a58e8917ad1c1b90864398ff74219d0aae0
Cédric Carré <cedric.carre@grtgaz.com>
2020-11-12 12:41:39 +0100

    Initial Commit Add

---
+++ src/main/java/com/cedriccarre/sfcdi/SfcDiApplication.java
@@ -0,0 +1,12 @@
+package com.cedriccarre.sfcdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfcDiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfcDiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/cedriccarre/sfcdi/SfcDiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.cedriccarre.sfcdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfcDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
